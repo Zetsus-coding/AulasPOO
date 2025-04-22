8f8ddeed60263a1ec51c769320b1788b9459ca88
alexr <alexr@DESKTOP-I9SIN13>
2025-04-22 01:03:07 -0300

    Commit onde as classes base e derivadas já estão "preparadas" (de forma simples) e a B.Genérica já tem a restrição. Ainda é necessário terminar de implementar para juntar com o main
    Alterações:
    
    1) Criação das classes ItensBiblioteca (Base); CD e MidiaDigital (herança as 2)
    2) Ajuste na classe Livro (herança)
    3) Ajuste na B.Genérica (restrição)
    4) Readme com a solução (p/ não esquecer)

---
+++ AulaPOO_15_04_25/CD.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AulaPOO_15_04_25
+{
+    public class CD : ItensBiblioteca
+	{
+        public string? Artista { get; set; }
+        public string Duracao { get; set; }
+    }
+}
---
+++ AulaPOO_15_04_25/ItensBiblioteca.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AulaPOO_15_04_25
+{
+    public class ItensBiblioteca
+	{
+		public int id {  get; set; }
+		public string? Titulo { get; set; }
+	}
+}
